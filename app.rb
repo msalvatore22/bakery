d2c5c07217ebae450ad05d892c7581b6e8d287e4
Michael Salvatore <msalvatore22@gmail.com>
2018-04-25 14:27:43 -0400

    form is working

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require "sinatra"
 require "sendgrid-ruby"
-
+include SendGrid
 
 get "/" do
   erb :home
+end
+
+post "/" do
+p	params.inspect
+
+from = Email.new(email: "msalvatore22@gmail.com")
+to = Email.new(email: params[:email])
+subject = "top muffin catalog"
+content = Content.new(type: 'text/plain', value: "muffins, cookies, cupcakes" )
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+redirect "/"
 end
